f03390f46341538da6dd5af10cee22e132c042ae
Jeremy Bissonnette <jeremy.kwmassage@gmail.com>
2022-06-16 08:10:54 -0400

    Remove React logo

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,10 @@ import React from 'react';
 
 import styled from 'styled-components';
 
-import logo from './logo.svg';
-
 export const App = () => (
   <sc.Container>
     <sc.Header>
-      <sc.Logo src={logo} alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <sc.Link
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer">
-        Forget React
-      </sc.Link>
-      <h2>number</h2>
+      <h1>Welcome</h1>
     </sc.Header>
   </sc.Container>
 );
@@ -36,14 +24,5 @@ const sc = {
     font-size: calc(10px + 2vmin);
     justify-content: center;
     min-height: 100vh;
-  `,
-
-  Link: styled.a`
-    color: #61dafb;
-  `,
-
-  Logo: styled.img`
-    height: 40vmin;
-    pointer-events: none;
   `,
 };
